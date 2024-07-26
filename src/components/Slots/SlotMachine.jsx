bb9c111abc19cd20625f0cbc32e848184d87abbe
Jhbalza2002 <josehumberto2002@gmail.com>
2024-07-26 12:48:11 -0500

    Slot Machine Integrated

--- src/components/Slots/SlotMachine.jsx
+++
@@ -1,7 +0,0 @@
-// import React from 'react'
-
-export default function SlotMachine() {
-  return (
-    <div>SlotMachine</div>
-  )
-}
--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./components/Home/Home.jsx";
 import Layout from "./components/Layout/Layout.jsx";
-import SlotMachine from "./components/Slots/SlotMachine.jsx";
+import SlotMachine from "./components/Slots/SlotMachine/SlotMachine.jsx";
 
 // const Blackjack = React.lazy(() => import('./components/Blackjack/Blackjack.jsx'));
 
